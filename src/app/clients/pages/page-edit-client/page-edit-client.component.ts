d6a53e2775b615c01679ef5d713bad711c387d7c
Carl GERARD <carl.gerard@epsi.fr>
2022-05-27 16:08:42 +0200

    Ajout formulaire "edit client"

--- src/app/clients/pages/page-edit-client/page-edit-client.component.ts
+++ src/app/clients/pages/page-edit-client/page-edit-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ClientsService} from "../../clients.service";
+import {Client} from "../../../core/models/client";
 
 @Component({
   selector: 'app-page-edit-client',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageEditClientComponent implements OnInit {
 
-  constructor() { }
+  public clientId = 0;
+  public client: Client = new Client;
+
+  constructor(private activatedRoute: ActivatedRoute, private clientService: ClientsService) { }
 
   ngOnInit(): void {
+    this.getClientByIdFromUrl()
   }
+
+  private getClient() {
+    this.clientService.getItemById(this.clientId).subscribe(
+      client => this.client = client
+    )
+    }
+    private getClientByIdFromUrl(){
+    this.activatedRoute.url.subscribe(
+      url => {
+        const id = url[url.length - 1].path;
+        this.clientId = Number(id);
+        this.getClient();
+      }
+    )
+    }
 
 }
